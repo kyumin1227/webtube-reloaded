3aa06e725b855853a5598d02440dfafeb3024c2e
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-02 22:27:18 +0900

    Report

--- src/server.js
+++ src/server.js
@@ -12,8 +12,18 @@ const handleLogin = (req, res) => {
     return res.send("Login here.");
 }
 
+const handleAbout = (req, res) => {
+  return res.send("<h1>About<h1>");
+};
+
+const handleContact = (req, res) => {
+  return res.send("<h1>Contact<h1>");
+};
+
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/about", handleAbout);
+app.get("/contact", handleContact);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
