c263c21e141a4566c9eb6c94cd139a5fe14e47f7
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-08 15:24:55 +0900

    Append PUG!

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,12 +1,9 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home");
 export const edit = (req, res) => {
     console.log(req.params);
     return res.send(`Edit Video #${req.params.id}`);
 };
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
-};
+export const see = (req, res) => { res.render("watch"); };  
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
