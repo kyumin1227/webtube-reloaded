4cfe8ee99865dfa586e7442a8d66d63d0449b934
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-11 01:27:10 +0900

    Iteration & Mixins

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -3,7 +3,34 @@ const fakeUser = {
     loggedIn: false,
 }
 
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            view: 59,
+            id: 1
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            view: 59,
+            id: 2
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            view: 59,
+            id: 3
+        },
+    ]
+    return res.render("home", { pageTitle: "Home", videos})};
 export const edit = (req, res) => {
     console.log(req.params);
     return res.send(`Edit Video #${req.params.id}`);
