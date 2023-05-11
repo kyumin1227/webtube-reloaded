c84150e81b4a8e6535d3f8ede0a6d63aa560121b
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-11 12:49:55 +0900

    Watch connect Edit

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -3,14 +3,13 @@ const fakeUser = {
     loggedIn: false,
 }
 
-export const trending = (req, res) => {
-    const videos = [
+const videos = [
         {
             title: "First Video",
             rating: 5,
             comments: 2,
             createdAt: "2 minutes ago",
-            view: 59,
+            view: 1,
             id: 1
         },
         {
@@ -30,12 +29,18 @@ export const trending = (req, res) => {
             id: 3
         },
     ]
+
+export const trending = (req, res) => {
     return res.render("home", { pageTitle: "Home", videos})};
 export const edit = (req, res) => {
     console.log(req.params);
     return res.send(`Edit Video #${req.params.id}`);
 };
-export const see = (req, res) => { res.render("watch"); };  
+export const watch = (req, res) => {
+    const id = req.params.id;
+    const video = videos[id - 1];
+    return res.render("Watch", { pageTitle: `Watching ${video.title}`, video });
+};  
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
