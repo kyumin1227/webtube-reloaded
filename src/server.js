75afb31ede01b193fdb8037e42ba82ed3d2c8de9
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-11 19:56:30 +0900

    Receive data for POST

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ app.use(logger);
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
