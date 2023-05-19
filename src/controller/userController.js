70f00f05b9b2f35564a32229e8da6ef0f3b0d50b
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-19 15:57:07 +0900

    Make a login page

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "Create Account" });
@@ -25,11 +26,38 @@ export const postJoin = async (req, res) => {
       errorMessage: "이미 등록된 이메일 입니다.",
     });
   }
-  await User.create({ name, username, email, password, location });
-  return res.redirect("/login");
+  try {
+    await User.create({ name, username, email, password, location });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "존재하지 않는 유저 이름입니다.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "비밀번호가 일치하지 않습니다.",
+    });
+  }
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("See User");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { home, search } from "../controller/videoController";
-import { getJoin, postJoin, login } from "../controller/userController";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controller/userController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
