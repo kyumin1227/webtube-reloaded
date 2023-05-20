25645b12a75a9f1e0b35d7349b9e7875ffb00a59
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-20 22:06:21 +0900

    Delete console.log locals

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,6 +2,5 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Wetube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.user;
-  console.log(res.locals);
   next();
 };
