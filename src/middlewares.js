b0562d5a5689b8285dd7b51cf66537a680ac5a7b
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-28 19:58:01 +0900

    Make protect middleware and protect route

--- src/middlewares.js
+++ src/middlewares.js
@@ -8,3 +8,21 @@ export const localsMiddleware = (req, res, next) => {
 };
 
 export const multerMiddleware = multer({ dest: "uploads/" });
+
+export const protectorMiddleware = (req, res, next) => {
+  // 로그인 한 사람만 들어갈 수 있게 설정
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  // 로그인 하지 않은 사람만 들어갈 수 있게 설정
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,15 +8,13 @@ import {
   finishGithubLogin,
 } from "../controller/userController";
 
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
+
 const userRouter = express.Router();
 
-// userRouter
-//   .route("/edit")
-//   .all(protectorMiddleware)
-//   .get(getEdit)
-//   .post(uploadFiles.single("avatar"), postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
 userRouter.get("/logout", logout);
 export default userRouter;
