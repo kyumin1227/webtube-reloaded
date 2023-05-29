1d9d3f260df520466fe18c74148a68c5ac82dfba
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-05-30 01:05:12 +0900

    Create a video controller

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,10 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volume = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 video.volume = 0.5;
 
@@ -39,6 +41,33 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const formatTime = (second) =>
+  new Date(second * 1000).toISOString().substring(11, 19);
+
+const handleLoadedMetadata = () => {
+  // 해당 video의 metadata들을 가져옵니다.
+  // video.duration = 해당 video의 길이
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+  timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  // video의 시간이 변경될 때 마다 호출됩니다.
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+  timeline.value = video.currentTime;
+};
+
+const handleVideoTime = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+  // video.currentTime = timeline.value;
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-volume.addEventListener("input", handleVolumeChange);
+volume.addEventListener("input", handleVolumeChange); // volume range를 움직이면 실행
+timeline.addEventListener("input", handleVideoTime); // timeline range를 움직이면 실행
+video.addEventListener("loadedmetadata", handleLoadedMetadata); // video의 메타데이터를 불러옴
+video.addEventListener("timeupdate", handleTimeUpdate); // video의 시간 변화를 감지
