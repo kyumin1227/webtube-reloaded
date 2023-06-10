6c083ee0d05a1c2fb58eeb0d97e197a6630e647e
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-10 23:30:16 +0900

    Fake comment create

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -5,6 +5,20 @@ const form = document.querySelector("#commentForm");
 const textarea = form.querySelector("textarea");
 const btn = form.querySelector("button");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  console.log(videoComments);
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  const span = document.createElement("span");
+  span.innerText = text;
+  icon.className = "fas fa-comment";
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment); // prepend = 가장 위에 추가
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const text = textarea.value;
@@ -13,7 +27,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json", // 해당 post는 json이라고 미들웨어에게 알려주는 역할
@@ -22,6 +36,10 @@ const handleSubmit = async (event) => {
       text,
     }),
   });
+  console.log(status);
+  if (status === 201) {
+    addComment(text);
+  }
   textarea.value = "";
 };
 
